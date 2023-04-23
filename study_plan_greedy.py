a0c83d401501a994d09f0b583d15e3abe3c8b020
Amit Priyansh Pare <pare.a@husky.neu.edu>
2023-04-23 02:26:34 +0000

    made a script to run for courses with levels

---
+++ study_plan_greedy.py
@@ -0,0 +1,160 @@
+import heapq
+import pprint as pp
+
+class CourseValue:
+    def __init__(self, course_dict):
+        self.course = course_dict["course"]
+        self.level = course_dict["level"]
+        self.hours = course_dict["hours"]
+        self.value = course_dict["value"]
+        self.hour_value_ratio = self.value / self.hours
+
+    def __lt__(self, other):
+        return self.hour_value_ratio > other.hour_value_ratio
+
+class CourseLevel:
+    def __init__(self, course_dict):
+        self.course = course_dict["course"]
+        self.level = course_dict["level"]
+        self.hours = course_dict["hours"]
+        self.value = course_dict["value"]
+
+        self.hour_value_ratio = self.value / self.hours
+    def __lt__(self, other):
+        return self.level < other.level
+
+def get_course_schedule(courses, total_hours):
+    result = []
+    course_heap_dict = {}
+    total_value_possible = 0
+    for course in courses:
+        total_value_possible += course["value"]
+        if course["course"] in course_heap_dict.keys():
+            course_heap_dict[course["course"]].append(CourseLevel(course))
+        else:
+            course_heap_dict[course["course"]] = [CourseLevel(course)]
+    course_heap = []
+    for heap in course_heap_dict.values():
+        heapq.heapify(heap)
+        course = heapq.heappop(heap)
+        course_heap.append(CourseValue({"course":course.course, "level":course.level, "hours":course.hours, "value":course.value}))
+    heapq.heapify(course_heap)
+    num_course_level = len(courses)
+    hours_invested = 0
+    max_value_acquired = 0
+    while hours_invested < total_hours and num_course_level>0:
+        max_value_course = heapq.heappop(course_heap)
+        time_left_for_interview = total_hours - hours_invested
+        if max_value_course.hours > time_left_for_interview:
+            #course_value_acquired = max_value_course.value * time_left_for_interview / course.hours
+            hours_invested += time_left_for_interview
+            max_value_acquired += round((time_left_for_interview / course.hours)*max_value_course.value, 2)
+            course_taken_dict = {
+                "course":max_value_course.course,
+                "level":max_value_course.level,
+                "hours_invested": time_left_for_interview,
+                "pcnt_course_value_acquired": round((time_left_for_interview / course.hours)*100, 2)
+            }
+        else:
+            hours_invested += max_value_course.hours
+            max_value_acquired += max_value_course.value
+            course_taken_dict = {
+                "course":max_value_course.course,
+                "level":max_value_course.level,
+                "hours_invested": max_value_course.hours,
+                "pcnt_course_value_acquired": 100
+            }
+        result.append(course_taken_dict)
+        num_course_level -= 1
+        if  len(course_heap_dict[max_value_course.course]) > 0:
+            course = heapq.heappop(course_heap_dict[max_value_course.course])
+            heapq.heappush(course_heap, CourseValue({"course":course.course, "level":course.level, "hours":course.hours, "value":course.value}))
+    pcnt_preparation = round((max_value_acquired / total_value_possible)*100,2)
+    return result, pcnt_preparation
+
+if __name__ == "__main__":
+    total_hrs = 250
+
+    courses = [
+        {
+            "course": "AWS",
+            "level": 1,
+            "hours": 40,
+            "value": 20
+        },
+        {
+            "course": "AWS",
+            "level": 2,
+            "hours": 50,
+            "value": 30
+        },
+        {
+            "course": "AWS",
+            "level": 3,
+            "hours": 60,
+            "value": 150
+        },
+        {
+            "course": "Python",
+            "level": 1,
+            "hours": 40,
+            "value": 100
+        },
+        {
+            "course": "Python",
+            "level": 2,
+            "hours": 50,
+            "value": 240
+        },
+        {
+            "course": "Python",
+            "level": 3,
+            "hours": 60,
+            "value": 20  # Less value for the interview since position doesn't require high level
+        },
+        {
+            "course": "ML",
+            "level": 1,
+            "hours": 40,
+            "value": 180
+        },
+        {
+            "course": "ML",
+            "level": 2,
+            "hours": 50,
+            "value": 200
+        },
+        {
+            "course": "ML",
+            "level": 3,
+            "hours": 60,
+            "value": 250
+        },
+        {
+            "course": "SQL",
+            "level": 1,
+            "hours": 40,
+            "value": 80
+        },
+        {
+            "course": "SQL",
+            "level": 2,
+            "hours": 50,
+            "value": 120
+        },
+        {
+            "course": "SQL",
+            "level": 3,
+            "hours": 60,
+            "value": 20  # Less value for the interview since position doesn't require high level
+        },
+
+    ]
+
+    study_plan, pcnt_preparation = get_course_schedule(courses, total_hrs)
+    print("Plan for the interview looks like below(time in hrs):")
+    pp.pprint(study_plan)
+    print(f"Percentage chance to crack the interview with {total_hrs}\
+ hours with the plan is: {pcnt_preparation}%")
+
+
